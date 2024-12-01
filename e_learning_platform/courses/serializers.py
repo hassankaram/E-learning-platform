fed7f4edbfeee6e870f10de846b82101a66b1ea3
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 23:16:44 +0200

    code improvements in course/serializers.py(added a specific format for created_at)

--- e_learning_platform/courses/serializers.py
+++ e_learning_platform/courses/serializers.py
@@ -9,8 +9,12 @@ class CategorySerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name', read_only=True)
     instructor_name = serializers.CharField(source='instructor.username', read_only=True)
-
+    created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
     class Meta:
         model = Course
         fields = ['id', 'title', 'description', 'instructor_name', 'category_name', 'category', 'created_at']
-        
+
+    def validate_title(self, value):
+        if Course.objects.filter(title=value).exists():
+            raise serializers.ValidationError("A course with this title already exists.")
+        return value
