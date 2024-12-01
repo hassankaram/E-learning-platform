167962974538157d188b167f435778f0284c7423
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 22:10:10 +0200

    create homepage and added homepage url to URLs.py

--- e_learning_platform/e_learning_platform/urls.py
+++ e_learning_platform/e_learning_platform/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/', include('dj_rest_auth.registration.urls')),
     path('api/users/', include('users.urls')),
