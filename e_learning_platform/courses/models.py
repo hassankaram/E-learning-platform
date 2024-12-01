128ae55b4fe370704d20140ee89d7cd4d8cf6178
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 19:19:44 +0200

    define category and course models in courses/models.py

--- e_learning_platform/courses/models.py
+++ e_learning_platform/courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Category(models.Modek):
+    name = models.CahrField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="courses")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="courses")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
