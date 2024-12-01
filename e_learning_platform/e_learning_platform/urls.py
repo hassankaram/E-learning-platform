5b9f63548eac84977dcaa0c830934993722fd418
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 19:43:16 +0200

    add category and  courses urls in e_learning_platform urls.py

--- e_learning_platform/e_learning_platform/urls.py
+++ e_learning_platform/e_learning_platform/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration')),
     path('api/users/', include('users.urls')),
+    path('api/courses/', include('courses.urls'),)
 ]
