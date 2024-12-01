6bb56a6ebd021fb792f0c30aaf3205eadff851a6
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 18:14:19 +0200

    create a custom user model,Update settings.py to use the custom user model

--- e_learning_platform/users/models.py
+++ e_learning_platform/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    is_instructor = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
