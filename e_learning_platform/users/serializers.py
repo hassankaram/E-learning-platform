60936ba69aa4e2f311248b1ef883ef322592ce96
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 18:21:35 +0200

    create a serializer file, create serializer for user registration

---
+++ e_learning_platform/users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.charField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'is_instructor', 'is_student']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            is_instructor=validated_data['is_instructor', False],
+            is_student=validated_data['is_student', True],
+        )
+        return user
