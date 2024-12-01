ff29889286b2e733f64ad27692252753777ccea5
Mahmoud Ahmed Elborm <m.ahmed2268@nu.edu.eg>
2024-12-01 18:45:47 +0200

    add URLs for authentication

--- e_learning_platform/e_learning_platform/urls.py
+++ e_learning_platform/e_learning_platform/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration'))
+    path('auth/registration/', include('dj_rest_auth.registration')),
+    path('api/users/', include('users.urls')),
 ]
