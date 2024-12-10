360a377bd1f073d65d8557da8e78347fca9f70b1
Mahmoud Ahmed <m.ahmed2268@nu.edu.eg>
2024-12-10 18:35:34 +0200

    create a view to allow students to enroll in courses

--- e_learning_platform/courses/views.py
+++ e_learning_platform/courses/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
-from .models import Category, Course
-from .serializers import CategorySerializer, CourseSerializer
 from rest_framework.permissions import IsAuthenticated
+from e_learning_platform.courses import serializers
+from .models import Category, Course, Enrollment
+from .serializers import CategorySerializer, CourseSerializer, EnrollmentSerializer
 from .permissions import IsInstructor
+
 
 class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -17,3 +19,12 @@ class CourseListView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(instructor=self.request.user)
 
+class EnrollmentListCreateView(generics.ListCreateAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_student:
+            raise serializers.ValidationError("Only students can enroll in courses.")
+        serializer.save(student=self.request.user)
