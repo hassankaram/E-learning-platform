8a8479e90392242782cee6d24e5341fddd16bea7
Mahmoud Ahmed <m.ahmed2268@nu.edu.eg>
2024-12-10 18:43:27 +0200

    updated url routing

--- e_learning_platform/courses/urls.py
+++ e_learning_platform/courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CategoryListView, EnrollmentListCreateView
+from .views import CategoryListView, CourseDetailView, EnrollmentListCreateView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('courses/', CategoryListView.as_view(), name='course-list'),
     path('enrollments/', EnrollmentListCreateView.as_view(), name='enrollments-list-create'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
 ]
 
 
