0ac0fc053cc92eb2509f71e1242fada68e5c9bda
Mahmoud Ahmed <m.ahmed2268@nu.edu.eg>
2024-12-10 18:22:17 +0200

    Create Enrollment Model

--- e_learning_platform/courses/models.py
+++ e_learning_platform/courses/models.py
@@ -56,3 +56,14 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Enrollment(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enrollments')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='enrollments')
+    date_enrolled = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('student', 'course')  # Prevent duplicate enrollments
+
+    def __str__(self):
+        return f"{self.student.username} enrolled in {self.course.title}"
