8c8c13f73d87f92cdd3bbdd4edf8757e00a5cd3d
Mahmoud Ahmed <m.ahmed2268@nu.edu.eg>
2024-12-10 18:59:01 +0200

    Add the url routes for submitting reviews and viewing reviews

--- e_learning_platform/courses/urls.py
+++ e_learning_platform/courses/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import CategoryListView, CourseDetailView, EnrollmentListCreateView
+from .views import CategoryListView, CourseDetailView, CourseReviewListView, EnrollmentListCreateView, ReviewListCreateView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('courses/', CategoryListView.as_view(), name='course-list'),
     path('enrollments/', EnrollmentListCreateView.as_view(), name='enrollments-list-create'),
     path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
+    path('courses/<int:course_id>/reviews/', CourseReviewListView.as_view(), name='course-reviews'),
+    path('reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
 ]
 
 
