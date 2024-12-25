f3cdbc342c26b82d33285ce6c85bb821405de020
3wes <m.ahmed2231@nu.edu.eg>
2024-12-26 00:06:19 +0200

    passing the backend to 7oda to fix the enrollments api endpoint

--- e_learning_platform/e_learning_platform/urls.py
+++ e_learning_platform/e_learning_platform/urls.py
@@ -1,7 +1,7 @@
 # filepath: /Users/mohamed3wes/new e-learning/E-learning-platform/e_learning_platform/e_learning_platform/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from courses.views import categories_list, courses_list  # Corrected import
+from courses.views import categories_list, courses_list , enrollment_list # Corrected import
 from users.views import LoginView
 
 urlpatterns = [
@@ -11,5 +11,7 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/categories/', categories_list, name='categories_list'),
     path('api/courses/', courses_list, name='courses_list'),
+    path('enrollments/', enrollment_list, name='enrollment_list'),
+
     path('', include('users.urls'))
 ]
