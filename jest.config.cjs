3b6099d04b7e8f463aa4901ae7f5d67237f68e92
3wes <m.ahmed2231@nu.edu.eg>
2024-12-26 14:41:21 +0200

    Stash changes

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
+  // jest.config.js
+
+
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  transform: {},
-};
